857ae4536293af94ca054c9571a4c07a03f00a00
LAPTOP-S37N1HI5\yeison <yeisoncalderonpoveda08@gmail.com>
2023-08-25 15:55:35 -0500

    Task List Page

---
+++ lib/app/model/task.dart
@@ -0,0 +1,10 @@
+class Task {
+
+  Task(this.title, {this.done = false});
+
+
+  final String title;
+  final bool done;
+
+
+}
--- lib/app/view/task_list/task_list_page.dart
+++ lib/app/view/task_list/task_list_page.dart
@@ -1,46 +1,94 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class TaskListPage extends StatefulWidget {
-  const TaskListPage({super.key});
+import '../../model/task.dart';
+import '../components/h1.dart';
+import '../components/shape.dart';
 
-  @override
-  State<TaskListPage> createState() => _TaskListPageState();
-}
+class TaskListPage extends StatelessWidget {
+  const TaskListPage({Key? key}) : super(key: key);
 
-class _TaskListPageState extends State<TaskListPage> {
-  int count = 0;
-
-  @override
-  void initState() {
-    // TODO: implement initState
-    super.initState();
-  }
-  @override
-  void dispose() {
-    // TODO: implement dispose
-    super.dispose();
-  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Center(child: Text('Task List')),
-        leading: GestureDetector(
-            onTap: (){
-              Navigator.of(context).pop();
-            },
-            child: Center(child: Text('Atras'))),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Container(
+            width: double.infinity,
+            color: Theme.of(context).colorScheme.primary,
+            child: Column(
+              children: [
+                Row(children: const [Shape()]),
+                Column(
+                  children: [
+                    const SizedBox(height: 100),
+                    Image.asset(
+                      'assets/tasks-list-image.png',
+                      width: 120,
+                      height: 120,
+                    ),
+                    const SizedBox(height: 16),
+                    const H1('Completa tus tareas', color: Colors.white),
+                    const SizedBox(height: 24),
+                  ],
+                )
+              ],
+            ),
+          ),
+          const Padding(
+            padding: EdgeInsets.only(left: 30, right: 30, top: 35),
+            child: H1('Tareas'),
+          ),
+          Expanded(
+              child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 30),
+            child: ListView.separated(
+              itemBuilder: (_, index) => TaskItem(taskList[index]),
+              separatorBuilder: (_, __) => const SizedBox(height: 16),
+              itemCount: taskList.length,
+            ),
+          ))
+        ],
       ),
-      body: Center(child: Text('Hola, el contador es $count'),),
       floatingActionButton: FloatingActionButton(
-        onPressed: (){
-          setState(() {
-            count++;
-          });
-        },
-        child: Icon(Icons.add),
+        onPressed: () {},
+        child: const Icon(Icons.add, size: 50),
       ),
     );
   }
 }
+
+class TaskItem extends StatelessWidget {
+  const TaskItem(this.task, {super.key});
+
+  final Task task;
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(21),
+      ),
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 21, vertical: 18),
+        child: Row(
+          children: [
+            Icon(
+              Icons.check_box_outline_blank,
+              color: Theme.of(context).colorScheme.primary,
+            ),
+            const SizedBox(width: 10),
+            Text(task.title),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+final taskList = <Task>[
+  Task('Sacar el perro'),
+  Task('Hacer la compra'),
+  Task('Electiva institucional'),
+];
